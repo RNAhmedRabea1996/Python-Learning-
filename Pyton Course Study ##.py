ff1d4abef19fff5f356940d6aaa1f366206c990a
RNAhmedRabea1996 <ahmedrabea2976@gmail.com>
2026-07-12 19:31:01 +0300

    Add Python Course Study Exercises

---
+++ Pyton Course Study ##.py
@@ -0,0 +1,303 @@
+#integer
+#name="ahmed"
+#string
+#age=43
+#float
+#gpa=8.5
+
+#print("may name is " + name)
+#print("my age is " +str(age) )
+#print("my gpa is " + str( gpa))
+##################################################
+#calculation
+#+
+#print(10+20)
+#_
+#print(10-20)
+#*
+#print(20*10)
+#/
+#print(20/10)
+
+#num1 = 10
+#num2 = 20
+#Sum = num1+num2
+#print(Sum)
+# % modulus
+#num%same num = zero
+#small num% big numb = small num
+#big num %small num =big num - small num then result -small num till zero or num cant be minus
+#print(10%20)
+#print(10%3)
+#########################################################################################
+#name= "ahmed rabea study python  "
+#age =45
+#gpa = 4.7
+#print(type(age))
+#print(name.upper())
+#print(name.capitalize())
+#print(name.title())
+###########################################################################################
+###########################################################################################
+
+#name = input("enter your name  ")
+#print("your name is "+ name
+########################################################################################
+#********************************calculator *******************************
+#print("| welcome to calculation app |\n")
+#num1 = float(input("enter first num : "))
+#num2 = float(input("enter second num : "))
+#print("---------------------------------")
+
+#print("Adding : "+ str(num1 +num2) )
+#print("substract : "+ str(num1 -num2) )
+#print("multiply : "+ str(num1 * num2) )
+#print("divide : "+ str(num1 /num2) )
+#print("modulus : "+ str(num1 %num2) )
+#&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+#comparsions
+# result = True
+# result = False
+#print(10!=6)
+#boolean :True  /  False
+# ==
+#><
+# <= >=
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$4
+# x=10
+# x+=10
+# x+=20
+# print(x)
+# result = True
+# result = False
+# x=5
+# y=10
+#and
+# or
+# not
+# print(not 10>50)
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#IF
+# name= "AHMED"
+# if name=="ahmed":
+#     print("your name is ahmed")
+# elif name=="ADAM":
+#     print("your name is ali")
+# else:
+#     print("can not find result")
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# num1=float(input("enter first number : "))
+# oper = input("choose the operation (+ , - , * , / , %) : ")
+# num2=float(input("enter second number : "))
+# if oper=="+" :
+#     result= num1+ num2
+#     print(num1, "+" ,num2, "=", result)
+# elif oper =="-":
+#     result =num1-num2
+#     print(num1,"-",num2,"=",result)
+# elif oper =="*":
+#     result =num1*num2
+#     print(num1,"*",num2,"=",result)
+# elif oper =="/":
+#     result =num1/num2
+#     print(num1,"/",num2,"=",result)
+# elif oper =="%":
+#     result =num1%num2
+#     print(num1,"%",num2,"=",result)
+# else:print("invalid operation",oper)
+# &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+# nested if
+# drink = input("what would you like to drink ? (cofee/ tea) : ")
+# if drink == "tea":
+#     sugar=input("do you want it with sugar or black ? ")
+#     if sugar=="black":
+#         print("black tea")
+#     elif sugar=="with sugar" :
+#         print("nice tea with sugar")
+#     else:
+#         print("sorry")
+#
+# elif drink== "cofee":
+#     type_of_cofee =input("what is your type of cofee(ice cofee or nescafee)?")
+#     if type_of_cofee == "nescafee":
+#         print("with foam is better")
+#     else:
+#         print("sorry")
+# ________________________________________________________________________
+# food = input("what you want to eat ? (meat / checken) : ")
+# if food =="meat" :
+#     sause =input("which sause you need on meat ? ( ranche or parpkue ): ")
+#     if sause=="ranche":
+#         print("white ranche will be nice")
+#     elif sause=="parpkue":
+#         print("parpkue black sause delicious")
+#     else:print("diffrent sause ")
+# elif food== "checken":
+#     checken_type =input("which type need ?(zinger/ broast/fahita /shawrma) :")
+#     if checken_type == "zinger":
+#         print("it will be spicy")
+#     elif checken_type=="broast":
+#         print("with garlic sause ")
+#     elif checken_type=="shawrma":
+#         print("with cheese sause ")
+# else:print("you can eat fish ")
+# xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\
+# username = input("enter your user name : ")
+# password = input("enter your password : ")
+#
+# if username == "ahmed" and password =="1234":
+#     print("login correct")
+# elif username == "ahmed" or password =="1234":
+#     print("almost correct but some wrong details")
+# else:print("wrong username and password")
+
+# has_invitaion = input("do you have ivitaion? yes/no :")
+# wearing_formal = input("are you wearing formal ? yes/ no :")
+# if has_invitaion=="yes" and wearing_formal=="yes" :
+#     print("you are welcome")
+# elif has_invitaion=="yes" or wearing_formal=="yes":
+#     print("okay we will let you in")
+# else:print("you can not enter")
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# students=["ahmed", "ali","islam", "essam"]
+# print(students[3])
+# list1=["ahmed","ali","essam","fadl","sayed"]
+# print(list1[0])
+# print(list1[1])
+# print(list1[2])
+# print(list1[-3])
+# print(list1)
+# fruits=["apple","banana","orange","grippe","manjo"]
+# fruits.insert(-2, "watermelon")
+# fruits.remove("manjo")
+# del fruits[0]
+# fruits.pop(1)
+# print(fruits)
+# ======================================================================
+# for num in range(1,10):
+#     print(num)
+# word="python"
+# for letter in word:
+#     print(letter)
+
+# total= 0
+# for i in range(1,11):
+#     total+=i
+#     print(i,total)
+# &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+# ahmed:5
+# i=1
+# while i<11:
+#     print(i)
+#     i+=1
+# i=0
+# while i <=10:
+#     print(i)
+#     i+=2
+# names=["ahmed","ali","eslam","salem"]
+# for name in names :
+#     print(name)
+# numbers=[2,4,4,6,8,9]
+# for num in numbers :
+#     print(num*2)
+# grades =[55,76,47,87,69,48,46]
+# new_grades =[]
+# for grade in grades:
+#     bouns =grade+5
+#     new_grades.append(bouns)
+# print("original grades: ",grades)
+# print("grade after bouns:  ",new_grades)
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#break /continue
+# names= ["ahmed ","ali","mostafa", "osama","stop","khalid"]
+# for name in names :
+#     if name=="stop":
+#
+#        print("breack the loop")
+#        break
+#     print(name)
+
+# for i in range(1,6):
+#     if i==3:
+#         continue
+#
+#     print(i)
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# def greet():
+#     print("hello ahmed")
+# greet()
+# greet()
+# print("aaaaaaaa")
+# def say_hello(name,age,id):
+#     print("hello",name)
+#     print("your age is " , age)
+#     print("your id is ",id)
+# say_hello("ahmed",33,24055)
+# def add (x,y):
+#     print("result =",x*y)
+# add(3,4)
+# def average(num1,num2,num3):
+#     total=num1+num2+num3
+#     average=total/3
+#     print("average",average)
+# average(55,66,43)
+# def display():
+#     print("ahmed")
+#     return "ahmed"
+# name = display()
+# print(name)
+# ----------------------------------------------------------------------------------------------
+# x = 5
+# if x >2 :
+#     print("bigger than2")
+#     print("still bigger")
+# print("done with 2")
+#
+# for i in range(5):
+#     print(i)
+#     if i>2 :
+#         print("bigger than2")
+#     print("done with i",i)
+# print("all done")
+# x = 1
+# if x <2 :
+#     print("small")
+# elif x< 10 :
+#     print("medium")
+# else:
+#     print("large")
+# print("all done ")
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# rawstr = input("enter anumber :")
+# try:
+#     ival =int(rawstr)
+# except:
+#     ival =-1
+
+# astr = "hello bob"
+# try:
+#     istr =int(astr)
+# except:
+#     istr=-1
+# print("first",istr)
+# astr="123"
+# try:
+#     istr = int(astr)
+# except:
+#     istr =-1
+# print("second",istr )
+# astr = 'Hello Bob'
+# istr = 0
+# try:
+#     istr = int(astr)
+# except:
+#     istr = -1
+# print(istr)
+# n = 5
+# while n > 0 :
+#     print(n)
+# print('All done')
+# tot = 0
+# for i in [5, 4, 3, 2, 1] :
+#     tot = tot + 1
+# print(tot)
